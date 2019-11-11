dcac43b93fd72560c3d2913a4a8357f2d23004f6
nomang <nomanbaig678@gmail.com>
2019-11-11 22:10:03 +0500

    ghjk

---
+++ resources/views/hhh.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>gjhgkj</title>
+</head>
+<body>
+	@extends('welcome')
+	@section('mnb')
+	@parent
+	@endsection
+	<h2>my name is nouman asghar baig this is my first page<h2>
+
+</body>
+</html>
